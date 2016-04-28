34d6eea9e1e0da45c15e0a57369bc93c7efe253a
Joshua Karjala-Svendsen <joshua@fluxuries.com>
2016-04-28 11:56:47 +0200

    also don't overwrite HOST env variable

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -25,18 +25,18 @@ USAGE
 wait_for()
 {
     if [[ $TIMEOUT -gt 0 ]]; then
-        echoerr "$cmdname: waiting $TIMEOUT seconds for $HOST:$WAIT_PORT"
+        echoerr "$cmdname: waiting $TIMEOUT seconds for $WAIT_HOST:$WAIT_PORT"
     else
-        echoerr "$cmdname: waiting for $HOST:$WAIT_PORT without a timeout"
+        echoerr "$cmdname: waiting for $WAIT_HOST:$WAIT_PORT without a timeout"
     fi
     start_ts=$(date +%s)
     while :
     do
-        (echo > /dev/tcp/$HOST/$WAIT_PORT) >/dev/null 2>&1
+        (echo > /dev/tcp/$WAIT_HOST/$WAIT_PORT) >/dev/null 2>&1
         result=$?
         if [[ $result -eq 0 ]]; then
             end_ts=$(date +%s)
-            echoerr "$cmdname: $HOST:$WAIT_PORT is available after $((end_ts - start_ts)) seconds"
+            echoerr "$cmdname: $WAIT_HOST:$WAIT_PORT is available after $((end_ts - start_ts)) seconds"
             break
         fi
         sleep 1
@@ -48,16 +48,16 @@ wait_for_wrapper()
 {
     # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
     if [[ $QUIET -eq 1 ]]; then
-        timeout $TIMEOUT $0 --quiet --child --host=$HOST --port=$WAIT_PORT --timeout=$TIMEOUT &
+        timeout $TIMEOUT $0 --quiet --child --host=$WAIT_HOST --port=$WAIT_PORT --timeout=$TIMEOUT &
     else
-        timeout $TIMEOUT $0 --child --host=$HOST --port=$WAIT_PORT --timeout=$TIMEOUT &
+        timeout $TIMEOUT $0 --child --host=$WAIT_HOST --port=$WAIT_PORT --timeout=$TIMEOUT &
     fi
     PID=$!
     trap "kill -INT -$PID" INT
     wait $PID
     RESULT=$?
     if [[ $RESULT -ne 0 ]]; then
-        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for $HOST:$WAIT_PORT"
+        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for $WAIT_HOST:$WAIT_PORT"
     fi
     return $RESULT
 }
@@ -68,7 +68,7 @@ do
     case "$1" in
         *:* )
         hostport=(${1//:/ })
-        HOST=${hostport[0]}
+        WAIT_HOST=${hostport[0]}
         WAIT_PORT=${hostport[1]}
         shift 1
         ;;
@@ -85,12 +85,12 @@ do
         shift 1
         ;;
         -h)
-        HOST="$2"
-        if [[ $HOST == "" ]]; then break; fi
+        WAIT_HOST="$2"
+        if [[ $WAIT_HOST == "" ]]; then break; fi
         shift 2
         ;;
         --host=*)
-        HOST="${1#*=}"
+        WAIT_HOST="${1#*=}"
         shift 1
         ;;
         -p)
@@ -126,7 +126,7 @@ do
     esac
 done
 
-if [[ "$HOST" == "" || "$WAIT_PORT" == "" ]]; then
+if [[ "$WAIT_HOST" == "" || "$WAIT_PORT" == "" ]]; then
     echoerr "Error: you need to provide a host and port to test."
     usage
 fi
