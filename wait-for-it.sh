f9a0dc44d38e04d2f8fbcd517d6c202682e896c8
Ville Skyttä <ville.skytta@iki.fi>
2017-11-01 09:03:33 +0200

    Use type -p instead of which

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -142,8 +142,7 @@ CHILD=${CHILD:-0}
 QUIET=${QUIET:-0}
 
 # check to see if timeout is from busybox?
-# check to see if timeout is from busybox?
-TIMEOUT_PATH=$(realpath $(which timeout))
+TIMEOUT_PATH=$(realpath $(type -p timeout))
 if [[ $TIMEOUT_PATH =~ "busybox" ]]; then
         ISBUSY=1
         BUSYTIMEFLAG="-t"
